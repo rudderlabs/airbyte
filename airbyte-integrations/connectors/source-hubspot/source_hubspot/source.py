5f169c79d09b991be529530c861f8f6b22cae936
Debanjan <debanjan.ganguly.98@gmail.com>
2023-03-03 12:10:09 +0530

    chore: reuse `retry_connection_handler`

--- airbyte-integrations/connectors/source-hubspot/source_hubspot/source.py
+++ airbyte-integrations/connectors/source-hubspot/source_hubspot/source.py
@@ -3,6 +3,7 @@
 #
 
 import logging
+from itertools import chain
 from typing import Any, Iterator, List, Mapping, MutableMapping, Optional, Tuple, Union
 
 import requests
@@ -15,9 +16,6 @@ from airbyte_cdk.sources.utils.schema_helpers import split_config
 from airbyte_cdk.utils.event_timing import create_timer
 from airbyte_cdk.utils.traced_exception import AirbyteTracedException
 from requests import HTTPError
-from source_hubspot.constants import API_KEY_CREDENTIALS
-from requests.adapters import HTTPAdapter
-from urllib3 import Retry
 from source_hubspot.streams import (
     API,
     Campaigns,
@@ -41,11 +39,12 @@ from source_hubspot.streams import (
     Owners,
     Products,
     PropertyHistory,
-    Quotes,
     SubscriptionChanges,
     TicketPipelines,
     Tickets,
     Workflows,
+    retry_connection_handler,
+    retry_after_handler,
 )
 
 
@@ -66,17 +65,19 @@ class SourceHubspot(AbstractSource):
         return alive, error_msg
 
     def get_granted_scopes(self, authenticator):
+        @retry_connection_handler(max_tries=5, factor=1)
+        @retry_after_handler(max_tries=3)
+        def _request_scopes(url):
+            response = requests.get(url=url)
+            response.raise_for_status()
+            return response.json()
+    
         try:
             access_token = authenticator.get_access_token()
             url = f"https://api.hubapi.com/oauth/v1/access-tokens/{access_token}"
-            with requests.Session() as s:
-                s.mount("https://api.hubapi.com/oauth/v1/access-tokens/", HTTPAdapter(max_retries=Retry(
-                    total=10, backoff_factor=2, allowed_methods=None, status_forcelist=[429, 500, 502, 503, 504])))
-                response = s.get(url=url)
-                response.raise_for_status()
-                response_json = response.json()
-                granted_scopes = response_json["scopes"]
-                return granted_scopes
+            response_json = _request_scopes(url)
+            granted_scopes = response_json["scopes"]
+            return granted_scopes
         except Exception as e:
             return False, repr(e)
 
@@ -122,10 +123,6 @@ class SourceHubspot(AbstractSource):
             Workflows(**common_params),
         ]
 
-        credentials_title = credentials.get("credentials_title")
-        if credentials_title == API_KEY_CREDENTIALS:
-            streams.append(Quotes(**common_params))
-
         api = API(credentials=credentials)
         if api.is_oauth2():
             authenticator = API(credentials=credentials).get_authenticator()
@@ -135,6 +132,12 @@ class SourceHubspot(AbstractSource):
             available_streams = [stream for stream in streams if stream.scope_is_granted(granted_scopes)]
             unavailable_streams = [stream for stream in streams if not stream.scope_is_granted(granted_scopes)]
             self.logger.info(f"The following streams are unavailable: {[s.name for s in unavailable_streams]}")
+            partially_available_streams = [stream for stream in streams if not stream.properties_scope_is_granted()]
+            required_scoped = set(chain(*[x.properties_scopes for x in partially_available_streams]))
+            self.logger.info(
+                f"The following streams are partially available: {[s.name for s in partially_available_streams]}, "
+                f"add the following scopes to download all available data: {required_scoped}"
+            )
         else:
             self.logger.info("No scopes to grant when authenticating with API key.")
             available_streams = streams
