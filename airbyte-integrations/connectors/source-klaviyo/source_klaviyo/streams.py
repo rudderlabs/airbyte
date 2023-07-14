0a7a71e6a9907e47d9dfc83c93dfcdd52e017f47
a-rampalli <rampalli@rudderstack.com>
2023-07-14 10:50:02 +0530

    chore: adds logs for next page token

--- airbyte-integrations/connectors/source-klaviyo/source_klaviyo/streams.py
+++ airbyte-integrations/connectors/source-klaviyo/source_klaviyo/streams.py
@@ -147,6 +147,9 @@ class IncrementalKlaviyoStream(KlaviyoStream, ABC):
         decoded_response = response.json()
         if decoded_response.get("next"):
             return {"since": decoded_response["next"]}
+        
+        data = decoded_response.get("data", [{}]) or [{}]
+        self.logger.info("Last timestamp -> " + str(data[-1].get("timestamp", "No timestamp")))
 
         return None
 
